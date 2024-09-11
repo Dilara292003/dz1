850664560ef69fe26b3216097c1618c6edf233b7
Dilara292003 <dil.zubajdullina@gmail.com>
2024-09-11 20:39:59 +0300

    One

---
+++ fourth/devsearch/projects/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from users.models import Profile
+
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    featured_image = models.ImageField(blank=True, default="default.jpg", upload_to="projects/%Y/%m/%d/")
+    demo_link = models.CharField(max_length=500, blank=True)
+    source_link = models.CharField(max_length=500, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created']
