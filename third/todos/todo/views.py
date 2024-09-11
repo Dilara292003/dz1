f5aa24cceddcdd3ba6b9f27f56c24eb4eee87833
Dilara292003 <dil.zubajdullina@gmail.com>
2024-09-11 20:39:52 +0300

    One

--- third/todos/todo/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def signupuser(request):
-    return render(request, 'todo/signupuser.html',)
-
-def currenttodos(request):
-    return render(request, 'todo/currenttodos.html',)
